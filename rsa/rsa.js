a466e6b0ee787a712d3c76572e642a0f80e84e6b
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2017-08-14 12:48:34 -0300

    :bear: example

--- rsa/rsa.js
+++ rsa/rsa.js
@@ -1,7 +1,22 @@
 var ursa = require('ursa');
-var key = ursa.generatePrivateKey(1024, 65537);
-var privkeypem = key.toPrivatePem();
-var pubkeypem = key.toPublicPem();
 
-console.log(privkeypem.toString('ascii'));
-console.log(pubkeypem.toString('ascii'));
+var message = 'wtfbbqomfglolwutyologtfortfmlolwthsoab';
+
+console.log('ORIGINAL', message);
+
+var privateKey = ursa.generatePrivateKey(1024, 65537);
+var buffer = new Buffer(message);
+
+var result = privateKey.encrypt(buffer);
+var encrypted = result.toString('hex');
+
+console.log('ENCRYPTED, MOTHERF****ER!', encrypted);
+
+var encryptedBuffer = new Buffer(encrypted, 'hex');
+var result = privateKey.decrypt(encryptedBuffer);
+var decrypted = result.toString();
+
+console.log('DECRYPTED, MOTHERF****ER!', decrypted);
+
+console.log('AND... ARE THEY EQUALS??', (decrypted === message));
+
